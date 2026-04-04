547bc358f1e6d9f9046b065f906403a89050ca68
tanonkim <robbins4bos@gmail.com>
2026-04-04 22:14:39 +0900

    Revert "feat : JMS ActiveMQ Connection Factory 설정"
    This reverts commit f8322a543220ffdd15333e33c43bd8299c10cac2.

--- src/main/java/com/message/global/config/ActiveMqConfig.java
+++ src/main/java/com/message/global/config/ActiveMqConfig.java
@@ -2,11 +2,11 @@ package com.message.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import jakarta.jms.ConnectionFactory;
 import jakarta.jms.Message;
 import jakarta.jms.Session;
 import jakarta.jms.TextMessage;
-import org.springframework.boot.activemq.autoconfigure.ActiveMQConnectionFactoryCustomizer;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
@@ -19,9 +19,20 @@ import org.springframework.jms.support.converter.MessageConverter;
 @Configuration
 public class ActiveMqConfig {
 
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Value("${spring.activemq.user}")
+    private String user;
+
+    @Value("${spring.activemq.password}")
+    private String password;
+
     @Bean
-    public ActiveMQConnectionFactoryCustomizer activeMQConnectionFactoryCustomizer() {
-        return factory -> factory.setTrustAllPackages(true);
+    public ActiveMQConnectionFactory connectionFactory() {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(user, password, brokerUrl);
+        factory.setTrustAllPackages(true);
+        return factory;
     }
 
     @Bean
@@ -59,16 +70,16 @@ public class ActiveMqConfig {
     }
 
     @Bean
-    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory, MessageConverter jacksonJmsMessageConverter) {
-        JmsTemplate template = new JmsTemplate(connectionFactory);
+    public JmsTemplate jmsTemplate(MessageConverter jacksonJmsMessageConverter) {
+        JmsTemplate template = new JmsTemplate(connectionFactory());
         template.setMessageConverter(jacksonJmsMessageConverter);
         return template;
     }
 
     @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, MessageConverter jacksonJmsMessageConverter) {
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(MessageConverter jacksonJmsMessageConverter) {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
+        factory.setConnectionFactory(connectionFactory());
         factory.setMessageConverter(jacksonJmsMessageConverter);
         factory.setConcurrency("1-5");
         factory.setSessionTransacted(true);
